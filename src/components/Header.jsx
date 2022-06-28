9fe5671f7733f0d4f6830a35592bae2c849a7b52
Evgeniy Salish <jeka5650962@gmail.com>
2022-06-28 16:21:16 +0300

    Приступил к разбивке на компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import logo from "../assets/img/logo.png";
+import cart from "../assets/img/cart.svg";
+import like from "../assets/img/like.svg";
+import profile from "../assets/img/profile.svg";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__logo logo">
+                <div className="logo__img">
+                    <img src={logo} width={40} height={40} alt="logo"/>
+                </div>
+                <div className="logo__text">
+                    <h2>REACT SNEAKERS</h2>
+                    <div className="logo__subtitle">Магазин лучших кроссовок</div>
+                </div>
+            </div>
+            <div className="header__info info">
+                <img src={cart} alt="cart"/>
+                <span>1205 руб.</span>
+                <img src={like} alt="like"/>
+                <img src={profile} alt="profile"/>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
